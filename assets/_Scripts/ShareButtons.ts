abe064f2af4c7ab75bbd96afe9dbdde50a2b0f13
Armīns Jeltajevs <theretr0doc@gmail.com>
2020-07-09 11:58:43 +0300

    Updated variable names

--- assets/_Scripts/ShareButtons.ts
+++ assets/_Scripts/ShareButtons.ts
@@ -12,25 +12,35 @@ export default class Share extends cc.Component {
 
     onLoad () {
         const message = "Pievienojies man, un apgūsti profesijas.";
-        const titlePrefix = "Topošās spēles nosaukums"
+        const title = "Topošās spēles nosaukums";
         const link = "https://www.game.com";
 
         this.node.on('click', function (button) {
 
             switch(button.node.name) {
                 case "Draugiem":{
-                    cc.sys.openURL(`https://www.draugiem.lv/say/ext/add.php?title=${encodeURIComponent(message)}&url=${encodeURIComponent(link)}&titlePrefix=${encodeURIComponent(titlePrefix)}`);
+                    cc.sys.openURL(`https://www.draugiem.lv/say/ext/add.php?title=${encodeURIComponent(message)}&url=${encodeURIComponent(link)}&titlePrefix=${encodeURIComponent(title)}`);
                     break;
                 }
                 case "Facebook": {
-                    cc.sys.openURL(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}`)
+                    cc.sys.openURL(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}&quote=${encodeURIComponent(message)}`)
                     break;
                 }
                 case "Instagram": {
                     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-                        cc.sys.openURL("instagram://camera")
+                        if (navigator.share) {
+                            navigator.share({
+                              title: title,
+                              text: message,
+                              url: link,
+                            })
+                              .then(() => console.log('Successful share'))
+                              .catch((error) => console.log('Error sharing', error));
+                          }
+                        // cc.sys.openURL("instagram://camera")
                     } else {
-                        cc.sys.openURL("https://www.instagram.com/");
+                        //Open web page
+                        button.enabled = false;
                     }
                     break;
                 }
@@ -38,17 +48,16 @@ export default class Share extends cc.Component {
                     cc.sys.openURL(`https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}%20${encodeURIComponent(link)}`);
                     break;
                 }
-                case "ShareBtn": {
-
-                    if (button.node._parent.y >= 200) {
-                        button.node._parent.runAction(cc.moveBy(1, 0, -185));
+                case "ShareButton": {
+                    if (button.node._parent.y >= Math.abs(231)) {
+                        button.node._parent.runAction(cc.moveBy(0.45, 0, -Math.abs(140)));
                     } else {
-                        button.node._parent.runAction(cc.moveBy(1, 0, 185));
+                        button.node._parent.runAction(cc.moveBy(0.45, 0, Math.abs(140)));
                     }
                     break;
                 }
             }
-            console.log('click on ' + button.name);
+            console.log(`Clicked on ${button.node.name}`);
         });
     }
     
