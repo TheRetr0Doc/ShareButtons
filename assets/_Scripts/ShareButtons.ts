4662472e825b8a4dd836afedaefbdb5e8107c2ca
Armīns Jeltajevs <theretr0doc@gmail.com>
2020-07-09 10:10:10 +0300

    Added sliding panel

--- assets/_Scripts/ShareButtons.ts
+++ assets/_Scripts/ShareButtons.ts
@@ -14,25 +14,37 @@ export default class Share extends cc.Component {
         const message = "Pievienojies man, un apgūsti profesijas.";
         const titlePrefix = "Topošās spēles nosaukums"
         const link = "https://www.game.com";
+
         this.node.on('click', function (button) {
 
-            switch(button.name) {
-                case "Draugiem<Button>":{
+            switch(button.node.name) {
+                case "Draugiem":{
                     cc.sys.openURL(`https://www.draugiem.lv/say/ext/add.php?title=${encodeURIComponent(message)}&url=${encodeURIComponent(link)}&titlePrefix=${encodeURIComponent(titlePrefix)}`);
                     break;
                 }
-                case "Facebook<Button>": {
+                case "Facebook": {
                     cc.sys.openURL(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}`)
                     break;
                 }
-                case "Instagram<Button>": {
-                    // if (mobile)
-                    //else
-                    cc.sys.openURL("https://www.instagram.com/");
+                case "Instagram": {
+                    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+                        cc.sys.openURL("instagram://camera")
+                    } else {
+                        cc.sys.openURL("https://www.instagram.com/");
+                    }
                     break;
                 }
-                case "Twitter<Button>": {
+                case "Twitter": {
                     cc.sys.openURL(`https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}%20${encodeURIComponent(link)}`);
+                    break;
+                }
+                case "ShareBtn": {
+
+                    if (button.node._parent.y >= 200) {
+                        button.node._parent.runAction(cc.moveBy(1, 0, -185));
+                    } else {
+                        button.node._parent.runAction(cc.moveBy(1, 0, 185));
+                    }
                     break;
                 }
             }
