c561e1f0eacf3355fffe3b8533a8cc4f569202e5
Armīns Jeltajevs <theretr0doc@gmail.com>
2020-08-19 11:55:17 +0300

    Removed unnecessary comments

--- assets/_Scripts/Share.js
+++ assets/_Scripts/Share.js
@@ -1,9 +1,3 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 const message = "Pievienojies man, un apgūsti profesijas.";
 const title = "Topošās spēles nosaukums";
 const link = "https://www.game.com";
